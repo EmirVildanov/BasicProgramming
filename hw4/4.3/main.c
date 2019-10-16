21569a42a90154b2d8aca9ad41ad86f255824955
Emir <reddog201030@gamil.com>
2019-10-16 20:01:45 +0300

    Added 4.3

---
+++ hw4/4.3/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int** createArray(int size)
+{
+    int** list = malloc(size * sizeof(int*));
+    for (int i = 0; i < size; ++i)
+    {
+        list[i] = (int*) malloc(size * sizeof(int));
+        printf("(New line) ");
+        for (int j = 0; j < size; ++j)
+        {
+            printf("Enter new element of the array : ");
+            scanf("%d", &list[i][j]);
+        }
+    }
+    return list;
+}
+
+void moveRight(int **list, int *currentElementIndex1, int *currentElementIndex2, bool direction, int length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        *currentElementIndex2 += 1;
+        printf("%d ", list[*currentElementIndex1][*currentElementIndex2]);
+    }
+}
+
+void moveUp(int **list, int *currentElementIndex1, int *currentElementIndex2, bool direction, int length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        *currentElementIndex1 -= 1;
+        printf("%d ", list[*currentElementIndex1][*currentElementIndex2]);
+    }
+}
+
+void moveLeft(int **list, int *currentElementIndex1, int *currentElementIndex2, bool direction, int length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        *currentElementIndex2 -= 1;
+        printf("%d ", list[*currentElementIndex1][*currentElementIndex2]);
+    }
+}
+
+void moveDown(int **list, int *currentElementIndex1, int *currentElementIndex2, bool direction, int length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        *currentElementIndex1 += 1;
+        printf("%d ", list[*currentElementIndex1][*currentElementIndex2]);
+    }
+}
+
+void moveNumber(int **list, int *currentElementIndex1, int *currentElementIndex2, bool direction, int length, int number)
+{
+    if (direction)
+    {
+        moveRight(list, currentElementIndex1, currentElementIndex2, direction, length);
+        moveUp(list, currentElementIndex1, currentElementIndex2, direction, length);
+
+    }
+    else
+    {
+        moveLeft(list, currentElementIndex1, currentElementIndex2, direction, length);
+        moveDown(list, currentElementIndex1, currentElementIndex2, direction, length);
+    }
+    if (length == number - 1)
+    {
+        moveRight(list, currentElementIndex1, currentElementIndex2, direction, length);
+    }
+}
+
+void printSpiral(int **list, int length)
+{
+    bool rightUpDirection = true;
+    int lineIndex = length / 2;
+    int columnIndex = length / 2;
+    printf("%d ", list[lineIndex][columnIndex]);
+    for (int i = 1; i < length; ++i)
+    {
+        moveNumber(list, &lineIndex, &columnIndex, rightUpDirection, i, length);
+        rightUpDirection = !rightUpDirection;
+    }
+}
+
+int main() {
+    int number = 0;
+    printf("Enter the number n : ");
+    scanf("%d", &number);
+    int **numbers = createArray(number);
+    printSpiral(numbers, number);
+}
