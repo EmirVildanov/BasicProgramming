dbd7da0d387b2929e34e5b0cfc4b8a66967038c6
Emir <reddog201030@gamil.com>
2019-10-26 16:37:03 +0300

    Edit 2.5

--- hw2/task5/2.5.c
+++ hw2/task5/2.5.c
@@ -2,18 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-void checkStrings(char firstString[], char secondString[])
+const int maxSize = 100;
+
+int* createIntArray(int size)
+{
+    int *list = malloc(size * sizeof(int));
+    for (int i = 0; i < size; ++i)
+    {
+        list[i] = 0;
+    }
+    return list;
+}
+
+char* createCharArray(int size)
+{
+    char *list = malloc(size * sizeof(char));
+    for (int i = 0; i < size; ++i)
+    {
+        list[i] = '\0';
+    }
+    return list;
+}
+
+void checkStrings(char *firstString, char *secondString)
 {
     int answer = 1;
-    int symbolsCount[256] = {0};
+    int *symbolsCount = createIntArray(256);
     int firstLength = strlen(firstString);
     int secondLength = strlen(secondString);
     if (firstLength == secondLength)
     {
         for (int i = 0; i < firstLength; ++i)
         {
-            int firstSymbolIndex = (int)(firstString[i]);
-            int secondSymbolIndex = (int)(secondString[i]);
+            int firstSymbolIndex = (firstString[i]) - '0';
+            int secondSymbolIndex = (int) (secondString[i]) - '0';
             symbolsCount[firstSymbolIndex] += 1;
             symbolsCount[secondSymbolIndex] -= 1;
         }
@@ -38,16 +60,19 @@ void checkStrings(char firstString[], char secondString[])
     {
         printf("Your strings consist of different letters");
     }
+    free(symbolsCount);
 }
 
 int main()
 {
-    char firstInput[100];
-    char secondInput[100];
+    char *firstInput = createCharArray(maxSize);
+    char *secondInput = createCharArray(maxSize);
     printf("Enter the first string : ");
     scanf("%s", firstInput);
     printf("Enter the second string : ");
     scanf("%s", secondInput);
     checkStrings(firstInput, secondInput);
+    free(firstInput);
+    free(secondInput);
     return 0;
 }
