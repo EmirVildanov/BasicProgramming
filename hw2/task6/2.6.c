3a097b89de0071d7f237b7a9949d30c0bb10dab1
Emir <reddog201030@gamil.com>
2019-10-26 16:28:21 +0300

    Edit 2.6

--- hw2/task6/2.6.c
+++ hw2/task6/2.6.c
@@ -3,35 +3,86 @@
 #include <string.h>
 #include <math.h>
 
-int cmp(const void *a, const void *b) 
+const int maxSize = 100;
+
+int* createIntArray(int size)
 {
-     return *(int*)a - *(int*)b;
-} 
+    int *list = malloc(size * sizeof(int));
+    for (int i = 0; i < size; ++i)
+    {
+        list[i] = 0;
+    }
+    return list;
+}
+
+char* createCharArray(int size)
+{
+    char *list = malloc(size * sizeof(char));
+    for (int i = 0; i < size; ++i)
+    {
+        list[i] = '\0';
+    }
+    return list;
+}
+
+void quickSort(int* numbers, int left, int right)
+{
+    if (left >= right)
+    {
+        return;
+    }
+    int mainstayElement = numbers[left];
+    int rememberLeft = left;
+    int rememberRight = right;
+    while (left < right)
+    {
+        while (numbers[left] < mainstayElement)
+        {
+            left += 1;
+        }
+        while (numbers[right] > mainstayElement)
+        {
+            right -= 1;
+        }
+        int temporary = numbers[left];
+        numbers[left] = numbers[right];
+        numbers[right] = temporary;
+        if (numbers[left] == numbers[right])
+        {
+            ++left;
+        }
+    }
+    quickSort(numbers, rememberLeft, right - 1);
+    quickSort(numbers, right + 1, rememberRight);
+}
  
-int returnNewNumber(char string[])
+int returnNewNumber(char* string)
 {
     int length = 0;
     length = strlen(string);
     int number = atoi(string);
-    int digits[length];
+    int *digits = createIntArray(length);
     for (int i = 0; i < length; ++i)
     {
         int tenPower = (int) pow(10, length - i - 1);
         digits[i] = number / tenPower;
         number = number % tenPower;
     }
-    qsort(digits, length, sizeof(int), cmp);
+    quickSort(digits, 0, length - 1);
     for (int i = 0; i < length; ++i)
     {
         printf("%d", digits[i]);
     }
+    free(digits);
 }
 
 int main()
 {
-    char input[30];
+    char *input = createCharArray(maxSize);
     printf("Enter the number : ");
-    scanf("%s", &input);
+    scanf("%s", input);
+    printf("The smallest number is : ");
     returnNewNumber(input);
+    free(input);
     return 0;
 }
