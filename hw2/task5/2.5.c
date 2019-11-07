db74ec0d860d69ae0154dd75032f7c0737abad09
Emir <reddog201030@gamil.com>
2019-11-07 19:57:19 +0300

    Edited functions

--- hw2/task5/2.5.c
+++ hw2/task5/2.5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 const int maxSize = 100;
 
@@ -24,43 +25,35 @@ char* createCharArray(int size)
     return list;
 }
 
-void checkStrings(char *firstString, char *secondString)
+bool checkStrings(char *firstString, char *secondString)
 {
-    int answer = 1;
-    int *symbolsCount = createIntArray(256);
+    bool answer = true;
+    int *symbolsCount = createIntArray(26);
     int firstLength = strlen(firstString);
     int secondLength = strlen(secondString);
     if (firstLength == secondLength)
     {
         for (int i = 0; i < firstLength; ++i)
         {
-            int firstSymbolIndex = (firstString[i]) - '0';
-            int secondSymbolIndex = (int) (secondString[i]) - '0';
+            int firstSymbolIndex = (int) (firstString[i]) - 'a';
+            int secondSymbolIndex = (int) (secondString[i]) - 'a';
             symbolsCount[firstSymbolIndex] += 1;
             symbolsCount[secondSymbolIndex] -= 1;
         }
-        for (int i = 0; i < 256; ++i)
+        for (int i = 0; i < 26; ++i)
         {
             if (symbolsCount[i] != 0)
             {
-                answer = 0;
-                break;
+                answer = false;
             }
         }
     }
     else
     {
-        answer = 0;
-    }
-    if (answer == 1)
-    {
-        printf("I may make up first string from the second and backwards");
-    }
-    else
-    {
-        printf("Your strings consist of different letters");
+        answer = false;
     }
     free(symbolsCount);
+    return answer;
 }
 
 int main()
@@ -71,7 +64,15 @@ int main()
     scanf("%s", firstInput);
     printf("Enter the second string : ");
     scanf("%s", secondInput);
-    checkStrings(firstInput, secondInput);
+    bool answer = checkStrings(firstInput, secondInput);
+    if (answer)
+    {
+        printf("I may make up first string from the second and backwards");
+    }
+    else
+    {
+        printf("Your strings consist of different letters");
+    }
     free(firstInput);
     free(secondInput);
     return 0;
