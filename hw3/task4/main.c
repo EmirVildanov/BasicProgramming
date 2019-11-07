4ad8ec5317611d56b1d6a18e3bfb6123fd6412b7
Emir <reddog201030@gamil.com>
2019-11-07 22:01:36 +0300

    Divided input and function

--- hw3/task4/main.c
+++ hw3/task4/main.c
@@ -11,43 +11,43 @@ char* createArray(int length)
     return list;
 }
 
-int checkAppropriateUsers(int numberOfUsers, int actions, int timer)
+void checkAppropriateUsers(int numberOfActions, int* numbers, int numberOfUsers, int actions, int timer, int* answer)
 {
-    int answer = 0;
-    for (int i = 0; i < numberOfUsers; ++i)
+    int numberOfAppropriateActions = 0;
+    for (int i = 0; i < numberOfActions; i += 2)
     {
-        int numberOfActions = 0;
-        int numberOfAppropriateActions = 0;
-        printf("Enter the number of actions : ");
-        scanf("%d", &numberOfActions);
-        int *numbers = malloc(numberOfActions * sizeof(int));
-        printf("Enter actions time periods : ");
-        for(int i = 0; i < numberOfActions; ++i)
+        if (numbers[i] <= timer)
         {
-            scanf("%d", &numbers[i]);
+            ++numberOfAppropriateActions;
         }
-        for (int i = 0; i < numberOfActions; i += 2)
-        {
-            if (numbers[i] <= timer)
-            {
-                ++numberOfAppropriateActions;
-            }
-        }
-        if (numberOfAppropriateActions == actions)
-        {
-            ++answer;
-        }
-        free(numbers);
     }
-    return answer;
+    if (numberOfAppropriateActions == actions)
+    {
+        *answer += 1;
+    }
 }
 
 int main() {
     int users = 0;
     int actions = 0;
     int timer = 0;
+    int numberOfActions = 0;
+    int *numbers = malloc(numberOfActions * sizeof(int));
+    int answer = 0;
     printf("Enter the values : ");
     scanf("%d %d %d", &users, &actions, &timer);
-    printf("The number of appropriate actions is : %d", checkAppropriateUsers(users, actions, timer));
+    for (int i = 0; i < users; ++i)
+    {
+        printf("Enter the number of actions : ");
+        scanf("%d", &numberOfActions);
+        printf("Enter actions time periods : ");
+        for(int i = 0; i < numberOfActions; ++i)
+        {
+            scanf("%d", &numbers[i]);
+        }
+        checkAppropriateUsers(numberOfActions, numbers, users, actions, timer, &answer);
+    }
+    printf("The number of appropriate actions is : %d", answer);
+    free(numbers);
     return 0;
 }
