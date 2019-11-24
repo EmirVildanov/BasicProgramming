2080afa925bad17768bb32bd76197d83ce7f46ed
Emir <reddog201030@gamil.com>
2019-11-24 11:49:50 +0300

    Deleted useless for cycle

--- hw3/task1/main.c
+++ hw3/task1/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int maxSize = 100;
-
 int* createArray(int size)
 {
     int* newList = malloc(size * sizeof(int));
@@ -20,22 +18,11 @@ void swap(int list[], int first, int second)
     list[second] = temporary;
 }
 
-void heapify(int list[], int length, int firstIndex, int secondIndex)
+void heapify(int list[], int length, int index)
 {
     for (int i = 0; i < length / 2; ++i) //first sort
     {
-        for (int j = 2 * i + firstIndex; j < length; ++j)
-        {
-            if (list[i] < list[j])
-            {
-                swap(list, i, j);
-            }
-        }
-    }
-    
-    for (int i = 0; i < length / 2; ++i) //second sort
-    {
-        for (int j = 2 * i + secondIndex; j < length; ++j)
+        for (int j = 2 * i + index; j < length; ++j)
         {
             if (list[i] < list[j])
             {
@@ -47,20 +34,22 @@ void heapify(int list[], int length, int firstIndex, int secondIndex)
 
 void heapSort(int list[], int length)
 {
-    heapify(list, length, 1, 2);
+    heapify(list, length, 1);
+    heapify(list, length, 2);
     for (int i = 1; i < length; ++i)
     {
         swap(list, 0, length - i);
-        heapify(list, length - i, 1, 2);
+        heapify(list, length - i, 1);
+        heapify(list, length - i, 2);
     }
 }
 
 int main()
 {
-    int *numbers = createArray(maxSize);
     int length = 0;
     printf("Enter the size of your array : ");
     scanf("%d", &length);
+    int *numbers = createArray(length);
     for (int i = 0; i < length; ++i)
     {
         printf("Enter new element of the array : ");
