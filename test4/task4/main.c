8f32c72dfb217d2d128bf0bfb22c69d5c30c1b35
EmirVildanov <reddog201010@gmail.com>
2020-04-28 22:36:50 +0500

    Added main.c code to check functionality

--- test4/task4/main.c
+++ test4/task4/main.c
@@ -2,7 +2,27 @@
 #include "queue.h"
 
 int main() {
-    QueueElement *queue = createQueue();
-
+    Queue *queue = createQueue();
+    if (isEmptyQueue(queue))
+    {
+        printf("Queue is empty\n");
+    }
+    pushFrontToQueue(5, queue);
+    if (!isEmptyQueue(queue))
+    {
+        printf("Queue is not empty\n");
+    }
+    pushBackToQueue(9, queue);
+    pushFrontToQueue(1, queue);
+    pushBackToQueue(7, queue);
+    pushBackToQueue(13, queue);
+    printf("Current size: %d\n", size(queue));
+    printf("%d ", popBackFromQueue(queue));
+    printf("%d ", popFrontFromQueue(queue));
+    printf("%d ", popBackFromQueue(queue));
+    printf("%d ", popFrontFromQueue(queue));
+    printf("%d \n", popBackFromQueue(queue));
+    printf("Current size: %d\n", size(queue));
+    deleteQueue(queue);
     return 0;
 }
